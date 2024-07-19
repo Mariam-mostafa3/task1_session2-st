9bf053d7f545b6251b49e6fca02963caa26fb4d0
mariam mostafa <125483866+Mariam-mostafa3@users.noreply.github.com>
2024-07-19 17:00:37 +0300

    Add files via upload

---
+++ function.c
@@ -0,0 +1,11 @@
+int global_var1=10;
+int global_var2=20;
+
+
+
+void function_globals() {
+
+global_var1 +=5;
+global_var2 -=10;
+}
+
---
+++ main.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+
+extern void function_globals();
+
+
+
+int main(){
+
+
+function_globals();
+
+
+
+return 0;
+}
